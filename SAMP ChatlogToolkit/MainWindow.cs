0f44a0d0aba3e67d7df1cbffb505cb062515c13c
Florian van Dillen <info@florianvandillen.nl>
2014-02-26 22:56:00 +0100

    Renamed form to a more descriptive title.

--- SAMP ChatlogToolkit/MainWindow.cs
+++ SAMP ChatlogToolkit/MainWindow.cs
@@ -11,9 +11,9 @@ using System.IO;
 
 namespace SAMP_ChatlogToolkit
 {
-    public partial class Form1 : Form
+    public partial class MainWindow : Form
     {
-        public Form1()
+        public MainWindow()
         {
             InitializeComponent();
         }
--- SAMP ChatlogToolkit/Program.cs
+++ SAMP ChatlogToolkit/Program.cs
@@ -16,7 +16,7 @@ namespace SAMP_ChatlogToolkit
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainWindow());
         }
     }
 }
